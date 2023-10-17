86c14c600d0adccb50eee3539f4758a776c9c9fe
Daniil Fajnberg <mail@daniil.fajnberg.de>
2023-10-17 14:22:05 +0200

    🚧 Add and document custom `__init__` parameters to `SyslogFormatter`

--- src/syslogformat/formatter.py
+++ src/syslogformat/formatter.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from logging import Formatter, LogRecord
+from logging import WARNING, Formatter, LogRecord
 from types import TracebackType
 from typing import Any, Mapping, Optional, Tuple, Type
 from typing_extensions import Literal
@@ -25,6 +25,7 @@ class SyslogFormatter(Formatter):
     [RFC 3164](https://datatracker.ietf.org/doc/html/rfc3164#section-4.1)
     for details about `syslog` standard.
     """
+
     def __init__(
         self,
         fmt: str | None = None,
@@ -33,8 +34,57 @@ class SyslogFormatter(Formatter):
         validate: bool = True,
         *,
         defaults: Mapping[str, Any] | None = None,
-    ):
-        """Validates the formatter construction arguments."""
+        facility: int = 0,
+        detail_threshold: int = WARNING,
+        prepend_level_name: bool = True,
+    ) -> None:
+        """
+        Validates the added formatter constructor arguments.
+
+        For details about the base class' constructor parameters, the official
+        [docs](https://docs.python.org/3/library/logging.html#logging.Formatter)
+        provide full explanations.
+
+        Args:
+            fmt (optional):
+                A format string in the given `style` for the logged output as a
+                whole. The possible mapping keys are drawn from the
+                `logging.LogRecord` object’s
+                [attributes]((https://docs.python.org/3/library/logging.html#logrecord-attributes).
+                If not specified, `'%(message)s | %(name)s'` is used. If any
+                custom string is passed here, the `detail_threshold` and
+                `prepend_level_name` arguments will be ignored.
+            datefmt (optional):
+                Passed through to the parent `__init__`
+            style (optional):
+                Passed through to the parent `__init__`
+            validate (optional):
+                Passed through to the parent `__init__`
+            defaults (optional):
+                Passed through to the parent `__init__`
+            facility (optional):
+                Used to calculate the number in the PRI part at the very start
+                of each log message. This argument is multiplied by 8 and added
+                to the numerical value of the severity that corresponds to the
+                log level of the message.
+                Details about accepted numerical values can be found in
+                [section 4.1.1](https://datatracker.ietf.org/doc/html/rfc3164#section-4.1.1)
+                of RFC 3164.
+            detail_threshold (optional):
+                Any log message with log level greater or equal to this value
+                will have information appended to it about the module, function
+                and line number, where the log record was made. The suffix will
+                have the form ` | {module}.{function}.{line}`.
+                Defaults to `logging.WARNING`.
+                If `fmt` is passed, this argument will be ignored.
+            prepend_level_name (optional):
+                If `True`, the log level name will be prepended to every log
+                message (but _after_ the `syslog` PRI part). The prefix will
+                have the form `{levelname} | ` (with a fixed width of 9
+                characters of the part before the `|`).
+                Defaults to `True`.
+                If `fmt` is passed, this argument will be ignored.
+        """
         raise NotImplementedError
 
     def formatException(self, ei: ExcInfo | Tuple[None, None, None]) -> str:
