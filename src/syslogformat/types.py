0f66bafec51d1d0105595419c15c37cb4bb19941
Daniil Fajnberg <mail@daniil.fajnberg.de>
2023-11-15 18:59:28 +0100

    🐛 Fix typing issues with constructor kwargs in older Python versions

--- src/syslogformat/types.py
+++ src/syslogformat/types.py
@@ -1,7 +1,8 @@
 """Custom type definitions/aliases."""
 
+import sys
 from logging import PercentStyle
-from typing import Any, Mapping, Optional, Union
+from typing import TYPE_CHECKING, Any, Mapping, Optional, Union
 from typing_extensions import Literal, NotRequired, TypedDict
 
 AnyMap = Mapping[str, Any]
@@ -16,25 +17,37 @@ LevelFmtMap = Mapping[LogLevel, str]
 """"""
 
 
-class FormatterKwargs(TypedDict, total=False):
-    """Constructor keyword-arguments of the [`logging.Formatter`][] class."""
+if TYPE_CHECKING and sys.version_info < (3, 10):
 
-    fmt: Optional[str]
-    """"""
-    datefmt: Optional[str]
-    """"""
-    style: StyleKey
-    """"""
-    validate: bool
-    """"""
-    defaults: Optional[AnyMap]
-    """"""
+    class FormatterKwargs(TypedDict, total=False):  # noqa: D101
+        fmt: Optional[str]
+        datefmt: Optional[str]
+        style: StyleKey
+        validate: bool
 
+    class StyleKwargs(TypedDict):  # noqa: D101
+        fmt: str
 
-class StyleKwargs(TypedDict):
-    """Constructor keyword-arguments of any `logging.PercentStyle` class."""
+else:
 
-    fmt: str
-    """"""
-    defaults: NotRequired[Optional[AnyMap]]
-    """"""
+    class FormatterKwargs(TypedDict, total=False):
+        """Constructor keyword-arguments of the [`logging.Formatter`][]."""
+
+        fmt: Optional[str]
+        """"""
+        datefmt: Optional[str]
+        """"""
+        style: StyleKey
+        """"""
+        validate: bool
+        """"""
+        defaults: Optional[AnyMap]
+        """"""
+
+    class StyleKwargs(TypedDict):
+        """Constructor keyword-arguments of any `logging.PercentStyle`."""
+
+        fmt: str
+        """"""
+        defaults: NotRequired[Optional[AnyMap]]
+        """"""
